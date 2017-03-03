9abe5c916c4739bbcd1bec46054ab7e40d9b4f67
pinoytech <forum.providence@yahoo.com>
2017-03-03 10:05:27 +0800

    flash messages

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :response
-  validates :answer, presence: true
+  validates :answer, presence: {message: 'can\'t be blank'}
 end
